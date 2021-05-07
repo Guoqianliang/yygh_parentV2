494524b3a4474095873dde410012c86240eb8394
gql <guoqianliang@foxmail.com>
2021-05-07 18:24:34 +0800

    5.7后台管理系统基本完成

--- service_gateway/src/main/java/com/gql/yygh/ServerGatewayApplication.java
+++ service_gateway/src/main/java/com/gql/yygh/ServerGatewayApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Description:
+ * @Description: Gateway网关
  * @author Guoqianliang
  */
 
